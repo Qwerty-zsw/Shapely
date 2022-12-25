b72b1beadcf67ef38245833febbd98031ecd1d44
Masoud Hajiloo <masoud1223z@gmail.com>
2022-12-25 22:17:55 +0330

    need btn

--- src/Components/main-parallax/main-parallax.jsx
+++ src/Components/main-parallax/main-parallax.jsx
@@ -4,7 +4,10 @@ const Parallax = () => {
   return (
     <div className="parallax-window position-relative">
         <div className='w-100 h-75 d-flex position-absolute justify-content-center align-items-center'>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, sit?</p>
+            <div className='d-flex flex-column text-center'>
+            <section className='fs-60'>We Change Everything WordPress</section>
+            <small className='mt-4'>This is the only WordPress theme you will ever want to use.</small>
+            </div>
         </div>
     </div>
   );
