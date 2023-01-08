9d8b59c731e1c6d361c5b18cfd630f3fa2ad53d5
Masoud Hajiloo <masoud1223z@gmail.com>
2023-01-08 13:01:03 +0330

    fixed btn transition

--- src/Components/button/Btn.jsx
+++ src/Components/button/Btn.jsx
@@ -1,15 +1,14 @@
-import { Button } from "react-bootstrap";
 import "./Btn.css";
 
 const Btnz = (props) => {
   return (
     <>
-      <Button
+      <button
         style={{ width: "11.5rem", padding: ".9rem" }}
-        className={`fs-7 fw-bold mx-1 border border-2 rounded-0 ${props.BtnSecClassName}`}
+        className={`btnz fs-7 fw-bold mx-1 border border-2 rounded-0 ${props.BtnSecClassName}`}
       >
         {props.BtnText}
-      </Button>
+      </button>
     </>
   );
 };
